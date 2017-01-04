56028afbd3e5537e60f1cfdfe29887ec0479cc75
chemhi <chemhi@hotmail.com>
2017-01-04 17:05:59 +0900

    Base Frame of the bulletin board with Jquery and Bootstrap

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var app = express();
+
+//DB Setting
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open", function(){
+  console.log("DB connected");
+});
+db.on("error", function(){
+  console.log("DB ERROR: ", err);
+});
+
+//Other Settings
+app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+//Routes
+app.use("/", require("./routes/home"));
+
+//port Setting
+app.listen(3000, function(){
+  console.log("server on!");
+});
